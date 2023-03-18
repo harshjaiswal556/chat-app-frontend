dd440581f8ad4004f867c24e8c996abb17fb3f97
harsh jaiswal <hsjaiswal3110@gmail.com>
2023-03-18 15:11:19 +0530

    Frontend of chat

---
+++ src/pages/ChatPage.js
@@ -0,0 +1,28 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import ChatBox from "../components/miscellaneous/ChatBox";
+import MyChats from "../components/MyChats";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import { ChatState } from "../context/ChatProvider";
+
+const ChatPage = () => {
+  const { user } = ChatState();
+
+  return (
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        width="100%"
+        height="91vh"
+        p="10px"
+      >
+        {user && <MyChats />}
+        {user && <ChatBox />}
+      </Box>
+    </div>
+  );
+};
+
+export default ChatPage;
